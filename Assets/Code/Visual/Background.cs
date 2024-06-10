680c2ba965649cc1dee165a3f3b1a49c8218e2dd
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-06-10 19:42:30 +0300

    Update Background.cs

--- Assets/Code/Visual/Background.cs
+++ Assets/Code/Visual/Background.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class Background : MonoBehaviour
 {
     [SerializeField] private Transform _target;
+    [SerializeField] private Transform[] _layers;
+    [SerializeField] private float _depth;
 
     private void FixedUpdate()
     {
-        transform.position = _target.position;
+        for(int i = 0; i < _layers.Length; i++)
+        {
+            var x = _target.position.x * 
+                ((i + _depth) / (float)(_layers.Length - 1 + _depth));
+
+            _layers[i].position = new Vector2(x, _target.position.y);
+        }
     }
 }
