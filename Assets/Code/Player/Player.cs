0f5b8aea7c7e0dae3d4a2de2e7c42e0458c17b50
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-06-10 20:27:43 +0300

    some test

--- Assets/Code/Player/Player.cs
+++ Assets/Code/Player/Player.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Movable))]
+[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
     private Movable _movable;
+    private Animator _animator;
+
+    private const string walkingBool = "walking";
+    private const string jumpingBool = "jumping";
+    private const string attackTrigger = "attack";
+    private const string dieTrigger = "die";
+    private const string hitTrigger = "hit";
+
+    private bool _isJumpingAnim = false;
 
     private void Start()
     {
         _movable = GetComponent<Movable>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -15,5 +26,36 @@ public class Player : MonoBehaviour
         _movable.LeftMoving = Input.GetKey(KeyCode.A);
         _movable.RightMoving = Input.GetKey(KeyCode.D);
         _movable.Jumping = Input.GetKey(KeyCode.W);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+
+        UpdateAnimator();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_movable.Jumping)
+        {
+            _isJumpingAnim = true;
+        }
+
+        if (_movable.IsGrounded)
+        {
+            _isJumpingAnim = false;
+        }
+    }
+
+    private void Attack()
+    {
+        _animator.SetTrigger(attackTrigger);
+    }
+
+    private void UpdateAnimator()
+    {
+        _animator.SetBool(walkingBool, _movable.LeftMoving | _movable.RightMoving);
+        _animator.SetBool(jumpingBool, _isJumpingAnim);
     }
 }
